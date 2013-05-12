25100e47af1114ec64a8473d085c7cf86c808a0e
HORINOUCHI Masato <thermes@confei.to>
2013-05-12 23:06:06 +0900

    [add] :define implement.

--- schemer.rb
+++ schemer.rb
@@ -20,7 +20,8 @@ def special_form?(exp)
   lambda?(exp) or
       let?(exp) or
       letrec?(exp) or
-      if?(exp)
+      if?(exp) or
+      define?(exp)
 end
 
 def lambda?(exp)
@@ -36,6 +37,8 @@ def eval_special_form(exp, env)
     eval_letrec(exp, env)
   elsif if?(exp)
     eval_if(exp, env)
+  elsif define?(exp)
+    eval_define(exp, env)
   end
 end
 
@@ -199,6 +202,51 @@ def list(*list)
   list
 end
 
+def eval_define(exp, env)
+  if define_with_parameter?(exp)
+    var, val = define_with_parameter_var_val(exp)
+  else
+    var, val = define_var_val(exp)
+  end
+  var_ref = lookup_var_ref(var, env)
+  if var_ref != nil
+    var_ref[var] = _eval(val, env)
+  else
+    extend_env!([var], [_eval(val, env)], env)
+  end
+  nil
+end
+
+def extend_env!(parameters, args, env)
+  alist = parameters.zip(args)
+  h     = Hash.new
+  alist.each { |k, v| h[k] = v }
+  env.unshift(h)
+end
+
+def define_with_parameter?(exp)
+  list?(exp[1])
+end
+
+def define_with_parameter_var_val(exp)
+  var              = car(exp[1])
+  parameters, body = cdr(exp[1]), exp[2]
+  val              = [:lambda, parameters, body]
+  [var, val]
+end
+
+def define_var_val(exp)
+  [exp[1], exp[2]]
+end
+
+def lookup_var_ref(var, env)
+  env.find { |alist| alist.key?(var) }
+end
+
+def define?(exp)
+  exp[0] == :define
+end
+
 $list_env = {
     :nil   => [],
     :null? => [:prim, lambda { |list| null?(list) }],
@@ -212,8 +260,10 @@ $boolean_env = {true: true, false: false}
 $global_env = [$list_env, $primitive_fun_env, $boolean_env]
 
 exp =
-    [:letrec,
-     [[:fact,
-       [:lambda, [:n], [:if, [:<, :n, 1], 1, [:*, :n, [:fact, [:-, :n, 1]]]]]]],
-     [:fact, 5]]
+    [:define, [:length, :list],
+     [:if, [:null?, :list],
+      0,
+      [:+, [:length, [:cdr, :list]], 1]]]
+exp2 = [:length, [:list, 1, 2]]
 puts _eval(exp, $global_env)
+puts _eval(exp2, $global_env)
